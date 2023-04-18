9438b2b7152236fc4175ed5055d93f1666f205de
dmk61 <dmk61@Dipak-Kadam>
2023-04-19 00:53:27 +0530

    commited first project

--- TestNG/src/com/basic/FirstScript.java
+++ TestNG/src/com/basic/FirstScript.java
@@ -1,10 +1,25 @@
 package com.basic;
 
-public class FirstScript {
 
-	public static void main(String[] args) {
-	
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class FirstScript {
+	static {
+		// Dipak
+		System.setProperty("webdriver.edge.driver", "C://msedgedriver.exe");
+		// dont create obj in static block you cant acess in local blcok
 
 	}
+	WebDriver driver = new EdgeDriver();
 
+	@Test
+	public void hello() {
+		System.out.print("hello testng");				
+
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.amazon.in/");
+
+	}
 }
